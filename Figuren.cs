5ba0b918131914615e77fad699b6c411b0deac99
NoortjeP <55238553+NoortjeP@users.noreply.github.com>
2023-01-27 19:40:06 +0100

    lk

--- Figuren.cs
+++ Figuren.cs
@@ -4,9 +4,12 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*Figuren is een nieuwe klasse met een constructormethode.
+Deze constructor maakt "Figuren". Dit is een object
+met wat voor soort tekening het is, het begin- en eindpunt 
+en de kleur. Deze objecten worden in de lijsten gebruikt.*/
 
-
-    public class Figuren
+public class Figuren
     {
         public string soort { get; set; }
         public Point beginpunt { get; set; }
